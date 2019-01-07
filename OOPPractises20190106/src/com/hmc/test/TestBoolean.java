8fc6bf4d32ce4f1d0631d2218f8db81cec108a29
meiceatgit <huangmc@dazhuanjia.com>
2019-01-07 12:57:16 +0800

    oop-1-practise

---
+++ OOPPractises20190106/src/com/hmc/test/TestBoolean.java
@@ -0,0 +1,14 @@
+package com.hmc.test;
+/**
+Author:Meice
+Date:2019年1月7日上午10:51:26
+**/
+public class TestBoolean {
+
+	
+	public static void main(String[] args) {
+		boolean b1 =  true;
+		boolean b2 = true;
+		System.out.println(b1 ==b2);
+	}
+}
